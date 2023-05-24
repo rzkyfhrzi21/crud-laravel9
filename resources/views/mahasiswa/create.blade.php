409e86c250b31412409ff768b56e32dcd97a5b3e
“RzkyFhrzi21” <rzkyfhrzi21@gmail.com>
2023-05-25 01:44:20 +0700

    initial repo

---
+++ resources/views/mahasiswa/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layout.template')
+ 
+@section('konten')
+
+<!-- START FORM -->
+ <form action='{{ url('mahasiswa')}}' method='post'>
+    @csrf
+    <div class="my-3 p-3 bg-body rounded shadow-sm">
+        <a href="{{ url('mahasiswa') }}" class="btn btn-secondary"><< Kembali</a>
+
+     <h4>CRUD Laravel 9 by Rizky Fahrezi</h4>
+     <div class="mb-3 row">
+         <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+         <div class="col-sm-10">
+             <input type="number" class="form-control" value="{{ Session::get('nim') }}" name='nim' id="nim">
+         </div>
+     </div>
+     <div class="mb-3 row">
+         <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+         <div class="col-sm-10">
+             <input type="text" class="form-control" value="{{ Session::get('nama') }}" name='nama' id="nama">
+         </div>
+     </div>
+     <div class="mb-3 row">
+         <label for="jurusan" class="col-sm-2 col-form-label">Jurusan</label>
+         <div class="col-sm-10">
+             <input type="text" class="form-control" value="{{ Session::get('jurusan') }}" name='jurusan' id="jurusan">
+         </div>
+     </div>
+     <div class="mb-3 row">
+         <label for="jurusan" class="col-sm-2 col-form-label"></label>
+         <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button></div>
+     </div>
+ 
+ </div>
+</form>
+ <!-- AKHIR FORM -->
+@endsection
+ 
